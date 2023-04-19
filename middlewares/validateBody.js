36de65b5636bb3a4707523aa2a7a8da532f30b27
Natalia Levenets <levenets.lot@gmail.com>
2023-04-19 20:55:45 +0300

    changed routes to MongoBD by using Mongoose req

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -6,10 +6,25 @@ const validateBody = schema => {
         if (error) {
             next(HttpError(400, error.message));
         }
-        next(error)
+        next()
+    }
+
+    return func;
+};
+
+const validateBodyFavorite = schema => {
+    const func = (req, res, next)=> {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            next(HttpError(400, error.message="missing field favorite"));
+        }
+        next()
     }
 
     return func;
 }
 
-module.exports = validateBody;
+module.exports = {
+    validateBody, 
+    validateBodyFavorite,
+};
